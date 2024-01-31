43070319ae574247c9e1fb87e393ae083a5c187c
NeLeEnGiChUkI <lovelyneleen@gmail.com>
2024-01-31 18:26:21 +0300

    update templates

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
+from flask import render_template 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'This is flask'
+    """Index URL"""
+    return render_template('index.html', title='index page')
