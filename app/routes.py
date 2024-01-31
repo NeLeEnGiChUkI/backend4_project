95c52d330a1c5f51bbfb151673a2ddbd7219a2b7
NeLeEnGiChUkI <lovelyneleen@gmail.com>
2024-01-31 18:58:24 +0300

    add another child template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from app import app
 from flask import render_template 
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     """Index URL"""
     return render_template('index.html', title='index page')
+
+
+@app.route('/about-me')
+def about_me():
+    """About me URL"""
+    return render_template('about.html', title='about-me')
